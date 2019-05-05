addde387af361c76dee57bec747515f6e5c4c031
Yuki Sumida <y.sumida.0201@gmail.com>
2019-05-05 23:59:03 +0900

    RSSフィードの内容を表示する

--- app/src/main/java/com/example/rssreader/MainActivity.kt
+++ app/src/main/java/com/example/rssreader/MainActivity.kt
@@ -2,11 +2,35 @@ package com.example.rssreader
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.LoaderManager
+import android.support.v4.content.Loader
+import android.support.v7.widget.GridLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<Rss> {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportLoaderManager.initLoader(1, null, this)
+    }
+
+    override fun onCreateLoader(p0: Int, p1: Bundle?) = RssLoader(this)
+
+    override fun onLoaderReset(p0: Loader<Rss>) {}
+
+    override fun onLoadFinished(p0: Loader<Rss>, p1: Rss?) {
+        if (p1 != null) {
+            val adapter = ArticlesAdapter(this, p1.articles) { article ->
+
+            }
+
+            articles.adapter = adapter
+
+            val layoutManager = GridLayoutManager(this, 2)
+
+            articles.layoutManager = layoutManager
+        }
     }
 }
