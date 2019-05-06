b2cd927e40d103424ddbd05df7d9d2587d43fd6c
Yuki Sumida <y.sumida.0201@gmail.com>
2019-05-06 19:47:04 +0900

    記事タップ時にChrome Custom Tabsで開く

--- app/src/main/java/com/example/rssreader/MainActivity.kt
+++ app/src/main/java/com/example/rssreader/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.rssreader
 
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.customtabs.CustomTabsIntent
 import android.support.v4.app.LoaderManager
 import android.support.v4.content.Loader
 import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<Rss> {
@@ -23,7 +26,8 @@ class MainActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<Rss> {
     override fun onLoadFinished(p0: Loader<Rss>, p1: Rss?) {
         if (p1 != null) {
             val adapter = ArticlesAdapter(this, p1.articles) { article ->
-
+                val intent = CustomTabsIntent.Builder().build()
+                intent.launchUrl(this, Uri.parse(article.link))
             }
 
             articles.adapter = adapter
